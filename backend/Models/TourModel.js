2c413470ec0658187c386f74152638dde4e6312f
HasanTayar <124785198+HasanTayar@users.noreply.github.com>
2023-04-19 16:41:01 +0300

    Restore deleted files

---
+++ backend/Models/TourModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Decimal128 = mongoose.Types.Decimal128;
+
+const TourSchema = new mongoose.Schema({
+    organizerId : { type: String, required: true },
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    photoTimeline: { type: String, required: true },
+    isPopular:{type:Boolean , default:false},
+    rating: { type: Decimal128 },
+    orderCount:{type:Number , default:0},
+    days: [
+      {
+        dayName: String,
+        photo: String,
+        location: String,
+        desc: String,
+      },
+    ],
+    location: [
+      {
+        locationName: String,
+        long: String,
+        lat: String,
+      },
+    ],
+    createdAt: { type: Date, default: Date.now },
+  });
+  
+
+const Tour = mongoose.model('Tour', TourSchema);
+module.exports = Tour;
